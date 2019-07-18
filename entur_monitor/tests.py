232584f252f26a60e9d52f24f77269fc548c04c2
datagutten <datagutten@datagutten.net>
2019-07-18 15:16:31 +0200

    Add travis testing

--- entur_monitor/tests.py
+++ entur_monitor/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.conf import settings
 
-# Create your tests here.
+
+class MonitorTests(TestCase):
+    INSTALLED_APPS = (
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.admin',
+    )
+
+    def setUp(self):
+        self.client = Client()
+
+        """settings.configure(
+            DEBUG=True,
+            # INSTALLED_APPS=self.INSTALLED_APPS + 'entur_monitor.apps.EnturMonitorConfig',
+            DATABASE=None,
+        )"""
+
+    def test_monitor(self):
+        response = self.client.get(
+            '/monitor/NSR:StopPlace:58381/NSR:Quay:8027,NSR:Quay:8028/Majorstua%20(Tunnelbane)' +
+            '/NSR:Quay:8050,NSR:Quay:8051/Majorstua%20(Sporvogn)?limit=15')
+        self.assertEqual(response.status_code, 200)
